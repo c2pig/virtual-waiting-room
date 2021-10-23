63e3ae2247ce5d1a47dca4062c9ff457dc8db762
c2pig <c2pig@c2pigs-iMac.local>
2021-10-23 23:09:31 +0800

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,5 @@
-import express, { Request, Response } from 'express';
+import type { HttpFunction } from '@google-cloud/functions-framework/build/src/functions';
 
-const app = express();
-
-const heroes = [
-  { id: 1, name: 'Iron Man' },
-  { id: 2, name: 'Thor' },
-  { id: 3, name: 'Black Widow' },
-  { id: 4, name: 'Hulk' },
-]
-
-app.get('/', async (req: Request, res: Response) => {
-  return { works: true }
-})
-
-app.get('/heroes', async (req: Request, res: Response) => {
-  return heroes
-})
-
-app.get('/heroes/:id', async (req: Request, res: Response) => {
-  const { id } = req.params
-  return heroes.find((h) => h.id === Number(id))
-})
-
-const port = 80;
-app.listen(port, () => {
-  console.log(`[Waiting Room Proxy] listening at http://localhost:${port}`)
-});
-
-exports.app = app;
+export const helloWorld: HttpFunction = (req, res) => {
+  res.send('Hello, World');
+};
